91721d606a2b719eafb31161c92dba0c167b5c26
LegacyBasketballAssociation <legacyybasketballassociation@gmail.com>
2025-07-20 14:01:43 -0700

    Update team_page.js

--- team_page.js
+++ team_page.js
@@ -1,4 +1,4 @@
-    // Toggle loading overlay
+// Toggle loading overlay
     const loadingOverlay = document.getElementById('loadingOverlay');
 
     function showLoading() {
@@ -19,21 +19,21 @@
             owner: "Kay Rivers",
             color: "#684428",
             logo: "https://drive.google.com/thumbnail?id=1E-WHv0i16L6ViK2AhLsY3tyduxo7j2nS&sz=w250-h180",
-            dataRow: 36,
+            dataRow: 38,
             dataRow2: 7,
-            dataRow3: 47,
-            dataRow4: 27,
-            dataRow5: 51
+            dataRow3: 49,
+            dataRow4: 17,
+            dataRow5: 53
         },
         DallasSteers: {
             name: "Dallas Steers",
             owner: "Keyon Hens",
             color: "silver",
             logo: "https://drive.google.com/thumbnail?id=1pIxyc3TwFKx0fEkhk7Tyc_ae4E0kRiuk&sz=w250-h180",
-            dataRow: 38,
-            dataRow2: 3,
-            dataRow3: 54,
-            dataRow4: 49,
+            dataRow: 40,
+            dataRow2: 5,
+            dataRow3: 11,
+            dataRow4: 51,
             dataRow5: 8
         },
         DenverMountaineers: {
@@ -42,20 +42,20 @@
             color: "#E1A80E",
             logo: "https://drive.google.com/thumbnail?id=1wkB6z3U6EwTGnYjad__52dR84KWMloL2&sz=w250-h180",
             dataRow: 24,
-            dataRow2: 33,
-            dataRow3: 22,
+            dataRow2: 35,
+            dataRow3: 13,
             dataRow4: 12,
-            dataRow5: 53
+            dataRow5: 57
         },
         MaineMobsters: {
             name: "Maine Mobsters",
             owner: "AK Abass",
             color: "gold",
             logo: "https://drive.google.com/thumbnail?id=1qZk64ky90r0Ddg_uhH7NOPQg5-vSHDjZ&sz=w250-h180",
-            dataRow: 5,
-            dataRow2: 43,
-            dataRow3: 19,
-            dataRow4: 11,
+            dataRow: 6,
+            dataRow2: 45,
+            dataRow3: 18,
+            dataRow4: 58,
             dataRow5: 31
         },
         MiamiViceSinners: {
@@ -63,21 +63,21 @@
             owner: "Kenan Kurt",
             color: "pink",
             logo: "https://drive.google.com/thumbnail?id=1qUSjUwrpWiHvExokwTkJWDCR0TZ9hlwT&sz=w250-h180",
-            dataRow: 37,
-            dataRow2: 17,
+            dataRow: 39,
+            dataRow2: 16,
             dataRow3: 9,
-            dataRow4: 20,
-            dataRow5: 16
+            dataRow4: 19,
+            dataRow5: 22
         },
         NewYorkCitySlickers: {
             name: "New York City Slickers",
             owner: "Maverick Doncic",
             color: "#263D4E",
             logo: "https://drive.google.com/thumbnail?id=1N0ZekAA21kXlQ9tnbyAseBePJJw79vy2&sz=w250-h180",
-            dataRow: 44,
-            dataRow2: 55,
-            dataRow3: 2,
-            dataRow4: 29,
+            dataRow: 46,
+            dataRow2: 59,
+            dataRow3: 3,
+            dataRow4: 62,
             dataRow5: 26
         },
         PhoenixHeat: {
@@ -85,22 +85,22 @@
             owner: "Razor Blades",
             color: "orange",
             logo: "https://drive.google.com/thumbnail?id=1h-vWWI_naDdsmKz2lsvfsPgFowBJQIWO&sz=w250-h180",
-            dataRow: 52,
-            dataRow2: 23,
-            dataRow3: 48,
-            dataRow4: 21,
-            dataRow5: 45
+            dataRow: 55,
+            dataRow2: 48,
+            dataRow3: 50,
+            dataRow4: 65,
+            dataRow5: 52
         },
         UtahRaptors: {
             name: "Utah Raptors",
             owner: "King Beowulf",
             color: "antiquewhite",
             logo: "https://drive.google.com/thumbnail?id=1zRbm7lk35HC9XMiOBEUhcBc_UMMhZ7t5&sz=w250-h180",
-            dataRow: 39,
-            dataRow2: 46,
-            dataRow3: 1,
-            dataRow4: 42,
-            dataRow5: 50
+            dataRow: 41,
+            dataRow2: 23,
+            dataRow3: 2,
+            dataRow4: 44,
+            dataRow5: 47
         }
     };
 
@@ -111,8 +111,8 @@
     function fetchRosterData() {
         showLoading();
 
-        const rosterUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQJJKiG4UE5nmSlrtKQ79eKhNaxqnWEUpwKMlOrmiCgRSBe9uFsJ1ywQGCkaNnt5BwfwySEcWzuEZpq/pub?gid=408327260&single=true&output=csv';
-        const statsUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSUXxv2LI_CXHUr7EQM5gX5ddG9fRA2oeMbfnC5_ruhTjWC2WjIYBhGJ91-qPoioyf2y5k5K6gBngEt/pub?gid=1444984150&single=true&output=csv';
+        const rosterUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQJJKiG4UE5nmSlrtKQ79eKhNaxqnWEUpwKMlOrmiCgRSBe9uFsJ1ywQGCkaNnt5BwfwySEcWzuEZpq/pub?gid=802365237&single=true&output=csv';
+        const statsUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSUXxv2LI_CXHUr7EQM5gX5ddG9fRA2oeMbfnC5_ruhTjWC2WjIYBhGJ91-qPoioyf2y5k5K6gBngEt/pub?gid=1692713008&single=true&output=csv';
 
         Promise.all([
                 new Promise(resolve => Papa.parse(rosterUrl, {
@@ -145,14 +145,14 @@
                         const ws = r[3] || 'N/A';
 
                         const playerStats = {
-                            ppg: cleanStat(s[2]),
-                            rpg: cleanStat(s[3]),
-                            apg: cleanStat(s[4]),
-                            spg: cleanStat(s[5]),
-                            bpg: cleanStat(s[6]),
-                            tov: cleanStat(s[7]),
-                            fg: cleanStat(s[8]),
-                            threeP: cleanStat(s[9])
+                            ppg: cleanStat(s[1]),
+                            rpg: cleanStat(s[2]),
+                            apg: cleanStat(s[3]),
+                            spg: cleanStat(s[4]),
+                            bpg: cleanStat(s[5]),
+                            tov: cleanStat(s[6]),
+                            fg: cleanStat(s[7]),
+                            threeP: cleanStat(s[8])
                         };
 
                         // Roster table row
